bd410c96cfb77b102331798743947b0ff6c1af93
angusbelove <66909583@qq.com>
2022-08-04 22:30:08 +0800

    second

--- kernel/proc.c
+++ kernel/proc.c
@@ -275,6 +275,8 @@ fork(void)
   }
   np->sz = p->sz;
 
+  np->mask = p->mask;
+
   np->parent = p;
 
   // copy saved user registers.
@@ -693,3 +695,29 @@ procdump(void)
     printf("\n");
   }
 }
+
+// Return the number of processes whose state is not UNUSED
+uint64
+nproc(void)
+{
+  struct proc *p;
+  // counting the number of processes
+  uint64 num = 0;
+  // traverse all processes
+  for (p = proc; p < &proc[NPROC]; p++)
+  {
+    // add lock
+    acquire(&p->lock);
+    // if the processes's state is not UNUSED
+    if (p->state != UNUSED)
+    {
+      // the num add one
+      num++;
+    }
+    // release lock
+    release(&p->lock);
+  }
+  return num;
+}
+
+
