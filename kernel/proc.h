43557c3f327b8cfd141730ba467f72c48a710c2c
angusbelove <66909583@qq.com>
2022-08-08 17:50:50 +0800

    ten

--- kernel/proc.h
+++ kernel/proc.h
@@ -82,25 +82,33 @@ struct trapframe {
 
 enum procstate { UNUSED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
-// Per-process state
+struct VMA {
+    uint64 addr;
+    uint64 length;
+    int prot;
+    int flags;
+    struct file *file;
+};
+
 struct proc {
-  struct spinlock lock;
+    struct spinlock lock;
 
-  // p->lock must be held when using these:
-  enum procstate state;        // Process state
-  struct proc *parent;         // Parent process
-  void *chan;                  // If non-zero, sleeping on chan
-  int killed;                  // If non-zero, have been killed
-  int xstate;                  // Exit status to be returned to parent's wait
-  int pid;                     // Process ID
+    // p->lock must be held when using these:
+    enum procstate state;        // Process state
+    struct proc *parent;         // Parent process
+    void *chan;                  // If non-zero, sleeping on chan
+    int killed;                  // If non-zero, have been killed
+    int xstate;                  // Exit status to be returned to parent's wait
+    int pid;                     // Process ID
 
-  // these are private to the process, so p->lock need not be held.
-  uint64 kstack;               // Virtual address of kernel stack
-  uint64 sz;                   // Size of process memory (bytes)
-  pagetable_t pagetable;       // User page table
-  struct trapframe *trapframe; // data page for trampoline.S
-  struct context context;      // swtch() here to run process
-  struct file *ofile[NOFILE];  // Open files
-  struct inode *cwd;           // Current directory
-  char name[16];               // Process name (debugging)
+    // these are private to the process, so p->lock need not be held.
+    uint64 kstack;               // Virtual address of kernel stack
+    uint64 sz;                   // Size of process memory (bytes)
+    pagetable_t pagetable;       // User page table
+    struct trapframe *trapframe; // data page for trampoline.S
+    struct context context;      // swtch() here to run process
+    struct file *ofile[NOFILE];  // Open files
+    struct inode *cwd;           // Current directory
+    char name[16];               // Process name (debugging)
+    struct VMA vmas[NVMA];
 };
